518e08ed0f9f61e86fea396f341cbeaad375914f
Olivia Kesler <oakesler@protonmail.ch>
2019-12-29 05:17:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
+    
+    get '/' do 
+      erb :new 
+    end
     # code other routes/actions here
     post '/pirates' do
       erb :show
